dd905433bc6bfaebfbf020881ac3dfebae6d736b
Angela Zhou <angelazhouqi@gmail.com>
2022-01-15 16:00:40 -0800

    Init

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, SafeAreaView, TouchableWithoutFeedback, Button } from 'react-native';
 
 export default function App() {
+
+  const handlePress = () => console.log("Text clicked");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>hm</Text>
+      <Button 
+        color="orange"
+        title="click me" 
+        onPress={() => console.log("clicked me")}/>
+      <Image source={require('./assets/favicon.png')}/>
+      <TouchableWithoutFeedback onPress={() => console.log("I")}>
+        <Image source={{
+        width: 200,
+        height: 300,
+        uri: "https://picsum.photos/200/300"}}/>
+      </TouchableWithoutFeedback>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
